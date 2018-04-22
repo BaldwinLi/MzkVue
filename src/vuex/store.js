6fb35c6dfea9850713846e863c26c769f09b2295
wei.k.li <wei.k.li@accenture.com>
2018-04-22 12:11:14 +0800

    update code for env

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -9,35 +9,37 @@ const proxyedHostNames = [
 ]
 
 export default new Vuex.Store({ // 名字自己定义
-  state: {
-    isLoading: false,
-    title: '首页'
-  },
-  mutations: {
-    updateLoadingStatus(state, payload) {
-      state.isLoading = payload.isLoading;
-    },
-    updateTitle(state, payload){
-      state.title = payload;
-    }
-  },
-  getters: {
-    isLocal: () => proxyedHostNames.includes(window.location.hostname),
-    appContextPath: () => proxyedHostNames.includes(window.location.hostname) ? `${window.location.origin}/dev_api/` : (window.location.origin + '/MzkAppService/'),
-  }
-}); // 这里你可能已经有其他 module
+      state: {
+        isLoading: false,
+        title: '首页'
+      },
+      mutations: {
+        updateLoadingStatus(state, payload) {
+          state.isLoading = payload.isLoading;
+        },
+        updateTitle(state, payload) {
+          state.title = payload;
+        }
+      },
+      getters: {
+        isLocal: () => proxyedHostNames.includes(window.location.hostname),
+        appContextPath: () => (proxyedHostNames.includes(window.location.hostname) ?
+          `${window.location.origin}/dev_api/` :
+          (window.location.origin + (window.location.protocol==='https:' ? '/MzkAppServiceTest/' : '/MzkAppService/'))),
+          }
+      }); // 这里你可能已经有其他 module
 
-// store.registerModule('vux', { // 名字自己定义
-//   state: {
-//     isLoading: false
-//   },
-//   mutations: {
-//     updateLoadingStatus(state, payload) {
-//       state.isLoading = payload.isLoading;
-//     }
-//   },
-//   getters: {
-//     isLocal: () => proxyedHostNames.includes(window.location.hostname),
-//     appContextPath: () => proxyedHostNames.includes(window.location.hostname) ? `${window.location.origin}/dev_api/` : (window.location.origin + '/'),
-//   }
-// });
+    // store.registerModule('vux', { // 名字自己定义
+    //   state: {
+    //     isLoading: false
+    //   },
+    //   mutations: {
+    //     updateLoadingStatus(state, payload) {
+    //       state.isLoading = payload.isLoading;
+    //     }
+    //   },
+    //   getters: {
+    //     isLocal: () => proxyedHostNames.includes(window.location.hostname),
+    //     appContextPath: () => proxyedHostNames.includes(window.location.hostname) ? `${window.location.origin}/dev_api/` : (window.location.origin + '/'),
+    //   }
+    // });
